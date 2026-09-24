72a5f008ed8f9ca80596e351d82f4969f5dcf59f
Student Naoise Quirke (TL_KCOMP_B_Y2) <t00271279@mymtu.ie>
2026-09-24 10:35:22 +0100

    Worked on the bomb scripts
    Signed-off-by: Student Naoise Quirke (TL_KCOMP_B_Y2) <t00271279@mymtu.ie>

--- Unity GD 26/Assets/RIchard Doyle/Scripts/RD_bombSlotScript.cs
+++ Unity GD 26/Assets/RIchard Doyle/Scripts/RD_bombSlotScript.cs
@@ -6,7 +6,7 @@ public class RD_bombSlotScript : MonoBehaviour
     RD_movementScript theBoss;
     internal void iAmTheBoss(RD_movementScript rD_movementScript)
     {
-        theBoss = RD_movementScript;
+        theBoss = rD_movementScript;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
